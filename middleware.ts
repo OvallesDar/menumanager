d7f10476dc9c6895ba4d73f36e7faac23cdb8a76
Darwin Ovalles <darwinovallesr@gmail.com>
2025-05-24 00:15:08 +0200

    cookieName Middleware

--- middleware.ts
+++ middleware.ts
@@ -2,18 +2,16 @@ import { getToken } from "next-auth/jwt"
 import { NextRequest, NextResponse } from "next/server";
 
 const protectedRoutes = ["/admin", "/dashboard" ];
-/* const authRoutes = ["/login"]; */
 const secret = process.env.AUTH_SECRET
 
 export default async function middleware(req: NextRequest) {
-  const token = await getToken({req, secret}) // La session de await auth(), si tiene fallos, rompe el middleware por eso uso token
+  const token = await getToken({req, secret, cookieName: "__Secure-authjs.session-token"}) // La session de await auth(), si tiene fallos, rompe el middleware por eso uso token
   const { nextUrl } = req;
 
   const isProtected = protectedRoutes.some((route) =>
     req.nextUrl.pathname.startsWith(route)
   );
 
-  /* const isAuthRoute = authRoutes.includes(nextUrl.pathname); */
   
   const res = NextResponse.next();
   res.headers.set('Access-Control-Allow-Origin', '*');
